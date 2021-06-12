ed13a9526b4b8d3650acbd02956b29b1c316140e
Carlos Eduardo Witte <cewitte@outlook.com>
2021-06-12 20:36:41 -0300

    Order, Instant, ISO 8601

--- src/main/java/cewitte/github/com/reference/repositories/UserRepository.java
+++ src/main/java/cewitte/github/com/reference/repositories/UserRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import cewitte.github.com.reference.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	
-
 }
