c9cce415694e3d0714cdb6423c8ca2a4582e710d
Carlos Eduardo Witte <cewitte@outlook.com>
2021-06-12 12:14:44 -0300

    Project created

---
+++ src/test/java/cewitte/github/com/reference/SpringBootJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package cewitte.github.com.reference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
